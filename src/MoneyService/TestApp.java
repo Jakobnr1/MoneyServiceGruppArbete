93c665a6dcedce17cc38bcaed42f422f25a4dee9
LindbergsKarl <karl.lindberg1@yaelev.se>
2021-04-10 10:14:40 +0200

    Update TestApp.java
    Ändrade lite i koden för att sätta upp boxen från start.

--- src/MoneyService/TestApp.java
+++ src/MoneyService/TestApp.java
@@ -15,44 +15,29 @@ public class TestApp {
 		//------------- Input from configuration file --------------
 		long boxId=22384;
 		MoneyBox theBox = new MoneyBox(boxId, currencyList); //Creating the box
+			
+		CurrencyName[] currencies = CurrencyName.values();
 		
-		List<Banknote> listOfNotes = new ArrayList<>(); 
-		
-		List<Banknote> listOfNotesSek = new ArrayList<>(); 
-		
-				CurrencyName currancyType = CurrencyName.SEK;
-		Banknote[] myNotes = new Banknote[5];
-		
-		myNotes[0] = new Banknote(20,30);
-		myNotes[1] = new Banknote(50,45);
-		myNotes[2] = new Banknote(100,68);
-		myNotes[3] = new Banknote(500,114);
-		myNotes[4] = new Banknote(1000,20);
-		
-		for(Banknote b: myNotes) {
-			listOfNotesSek.add(b);
+		//Setting up the box with currencies from start. 
+		for(CurrencyName c : currencies) {
+			CurrencyName currancyType = c;
+			List<Banknote> tempListOfNotes = new ArrayList<>();
+			
+			Banknote[] myNotes = new Banknote[5];
+			
+			myNotes[0] = new Banknote(20,0);
+			myNotes[1] = new Banknote(50,0);
+			myNotes[2] = new Banknote(100,0);
+			myNotes[3] = new Banknote(500,0);
+			myNotes[4] = new Banknote(1000,0);
+			
+			for(Banknote b: myNotes) {
+				tempListOfNotes.add(b);
+			}
+			
+			MoneyBox.addCurrencyToBoxAtStartOfDay(tempListOfNotes, currencyList, currancyType );
 		}
-		listOfNotes=listOfNotesSek;
-		
-		MoneyBox.addCurrencyToBoxAtStartOfDay(listOfNotes, currencyList, currancyType );
-		
-		
-		List<Banknote> listOfNotesEur = new ArrayList<>(); 
-		currancyType = CurrencyName.EUR;
-		myNotes = new Banknote[5];
-		
-		myNotes[0] = new Banknote(20,20);
-		myNotes[1] = new Banknote(50,25);
-		myNotes[2] = new Banknote(100,58);
-		myNotes[3] = new Banknote(500,189);
-		myNotes[4] = new Banknote(1000,22);
-		
-		for(Banknote b: myNotes) {
-			listOfNotesEur.add(b);
-		}
-		listOfNotes=listOfNotesEur;
-		MoneyBox.addCurrencyToBoxAtStartOfDay(listOfNotes, currencyList, currancyType );
-		//------------------------------------------
+
 		
 		int test = MoneyBox.getNumberOfNotes(currencyList, "SEK" , 100);
 		System.out.format("Number of 100 SEK notes in box at start of day: %d", test);
