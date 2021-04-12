7444d6f554fec5f1c1805cf5331873279c59aa92
Johannes <Johannes_Ahlstrom@hotmail.com>
2021-04-12 17:26:38 +0200

    Added EXRate,IO and Config.txt

--- src/MoneyService/Conifg.java
+++ src/MoneyService/Conifg.java
@@ -1,9 +1,27 @@
 package MoneyService;
 
-public class Conifg {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Conifg {
+	//Attributes
 	private static int MIN_AMMOUNT = 1;
 	private static int MAX_AMMOUNT = 2000;
-
-
+	public static List<ExchangeRate> exchangeRateList = new ArrayList<ExchangeRate>();
+	
+	//Methods
+	public List<ExchangeRate> getExchangeRateList(){
+		return this.exchangeRateList;
+	}
+	
+	public boolean setMIN_AMMOUNT(int MIN_AMMOUNT) {
+		this.MIN_AMMOUNT = MIN_AMMOUNT;
+		return true;
+	}
+	
+	public boolean setMAX_AMMOUNT(int MAX_AMMOUNT) {
+		this.MAX_AMMOUNT = MAX_AMMOUNT;
+		return true;
+	}
+	
 }
---
+++ src/MoneyService/MoneyServiceIO.java
@@ -0,0 +1,36 @@
+package MoneyService;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class MoneyServiceIO {
+
+	static String configFileName = "Config.txt";
+
+	public static ExchangeRate parseConfigRates(String StringToBeParsed) {
+		String[] parts = StringToBeParsed.split("=");
+		Float rate = Float.parseFloat(parts[1].trim());
+		ExchangeRate parsed = new ExchangeRate(parts[0].trim(),rate);
+		if(parsed.currencyName != null && parsed.exchangeRate != null) {
+			return parsed;
+		}
+		else {
+			throw new IllegalArgumentException(String.format("Error while parsing ExchangeRates"));
+		}
+	}
+
+	public static boolean readConfigTextFile() {
+		boolean read = false;
+		try(BufferedReader bf = new BufferedReader(new FileReader(configFileName))) {
+			while(bf.ready()) {
+				String temporaryString = bf.readLine();
+				ExchangeRate parsedRate = parseConfigRates(temporaryString);
+				Conifg.exchangeRateList.add(parsedRate);
+			}
+		}catch(IOException ioe) {System.out.println("Exception when reading file");}
+		read = true;
+		return read;
+	}
+
+}
