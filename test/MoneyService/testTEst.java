87ba1f2bf906f9a8c7ffc57c0408be8338346857
LindbergsKarl <karl.lindberg1@yaelev.se>
2021-04-12 11:45:05 +0200

    nytt försök med test

---
+++ test/MoneyService/testTEst.java
@@ -0,0 +1,14 @@
+package MoneyService;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testTEst {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
