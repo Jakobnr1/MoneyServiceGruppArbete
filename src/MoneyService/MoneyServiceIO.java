23928e1ea236e241e290a2401185e3be58eb66e7
Johannes <Johannes_Ahlstrom@hotmail.com>
2021-04-12 23:11:36 +0200

    Finished MoneyProjectIO

--- src/MoneyService/MoneyServiceIO.java
+++ src/MoneyService/MoneyServiceIO.java
@@ -1,14 +1,31 @@
 package MoneyService;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Map;
 
 public class MoneyServiceIO {
 
-	static String configFileName = "Config.txt";
+	static String configFilename = "Config.txt";
+	static String serializedDailyTransactionFilename = "DailyTransactions.ser";
+	static String serializedCustomerDataBaseFilename = "CustomerDatabase.ser";
+	static String textFormattedDailyTransactions = "DailyTransactions.txt";
+	
 
-	public static ExchangeRate parseConfigRates(String StringToBeParsed) {
+	/**
+	 * Helping method for parsing configuration data.
+	 * @param StringToBeParsed
+	 * @return
+	 */
+	public static ExchangeRate parseConfig(String StringToBeParsed) {
 		String[] parts = StringToBeParsed.split("=");
 		Float rate = Float.parseFloat(parts[1].trim());
 		ExchangeRate parsed = new ExchangeRate(parts[0].trim(),rate);
@@ -20,17 +37,101 @@ public class MoneyServiceIO {
 		}
 	}
 
+	/**
+	 * Reads the configuration file and parses the ExchangeRates into the configuration
+	 * @return boolean true if read.
+	 */
 	public static boolean readConfigTextFile() {
 		boolean read = false;
-		try(BufferedReader bf = new BufferedReader(new FileReader(configFileName))) {
+		try(BufferedReader bf = new BufferedReader(new FileReader(configFilename))) {
 			while(bf.ready()) {
 				String temporaryString = bf.readLine();
-				ExchangeRate parsedRate = parseConfigRates(temporaryString);
+				ExchangeRate parsedRate = parseConfig(temporaryString);
 				Config.exchangeRateList.add(parsedRate);
 			}
 		}catch(IOException ioe) {System.out.println("Exception when reading file");}
 		read = true;
 		return read;
 	}
+	
+	/**
+	 * Saves the daily transactions in serialized format
+	 * @param listToBeSaved
+	 * @return boolean true if done
+	 */
+	public static boolean saveSerializedDailyTransactions(List<Transaction> listToBeSaved) {
+		boolean saved = false;
+		try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(serializedDailyTransactionFilename))){
+			oos.writeObject(listToBeSaved);
+		}
+		catch(IOException ioe) {System.out.println(String.format("Error when saving serialized daily transaction"+ioe.toString()));}
+		saved = true;
+		return saved;
+	}
+	
+	/**
+	 * Reads Serialized daily transactions
+	 * @return the list
+	 */
+	
+	public static List<Transaction> readSerializedDailyTransactionList(){
+		List<Transaction> transactionList = null;
+		try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(serializedDailyTransactionFilename))){
+		transactionList = (List<Transaction>)ois.readObject();
+		}
+		catch(IOException |ClassNotFoundException ioe) {System.out.println(String.format("Error when reading serialized daily transactions"+ioe.toString()));
+		}
+		return transactionList;
+	}
+	
+	/**
+	 * Saves daily transactions as List
+	 * @param dailyList
+	 * @return boolean true if successful.
+	 */
+	public static boolean saveDailyTransactionListAsText(List<Transaction> dailyList){
+		boolean saved = false;
+		try(PrintWriter pw = new PrintWriter(new FileWriter(textFormattedDailyTransactions))){
+			for(Transaction t:dailyList) {
+				pw.println(t.toString());
+			}
+		}
+			catch(IOException ioe) {System.out.println(String.format("Error when Saving Daily Transactions as text" + ioe.toString()));
+		}
+		saved = true;
+		return saved;
+		
+	}
+
+	/**
+	 * Saves serialized customer database 
+	 * @param mapToBeSaved
+	 * @return boolean true if done
+	 */
+	
+	public static boolean saveSerializedCustomerDatabase(Map<String,List<Customer>> mapToBeSaved) {
+		boolean saved = false;
+		try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(serializedCustomerDataBaseFilename))){
+			oos.writeObject(mapToBeSaved);
+		}
+		catch(IOException ioe) {System.out.println(String.format("Error when saving serialized daily transaction"+ioe.toString()));}
+		saved = true;
+		return saved;
+	}	
+	
+	/**
+	 * Reads serialized customer database 
+	 * @return the map
+	 */
+	public static Map<String,List<Customer>> readSerializedCustomerDatabase(){
+		Map<String,List<Customer>> customerMap = null;
+		try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(serializedCustomerDataBaseFilename))){
+		customerMap = (Map<String,List<Customer>>)ois.readObject();
+		}
+		catch(IOException |ClassNotFoundException ioe) {System.out.println(String.format("Error when reading serialized daily transactions"+ioe.toString()));
+		}
+		return customerMap;
+	}
+	
 
 }
---
+++ test/MoneyService/testExchangeRate.java
@@ -0,0 +1,42 @@
+package MoneyService;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testExchangeRate {
+
+	Float rate = (float) 4.50;
+	ExchangeRate x = new ExchangeRate("GBP/JPY", rate);
+	
+	@Test
+	public void testConstructor() {
+		ExchangeRate y = new ExchangeRate("SEK/EUR",4.02F);
+		assertNotNull(y);
+	}
+	
+	@Test
+	public void testGetName() {
+		assertEquals(x.currencyName, "GBP/JPY");
+	}
+	
+	@Test
+	public void testSetName() {
+		x.setName("Test");
+		assertEquals(x.getName(),"Test");
+	}
+	
+	@Test
+	public void testGetExchangeRate() {
+		assertEquals(x.getExchangeRate(), rate);
+	}
+	
+	@Test
+	public void testSetExchangeRate() {
+		x.setExchangeRate(4.0F);
+		assertNotNull(x.exchangeRate);
+	}
+	
+	
+
+}
