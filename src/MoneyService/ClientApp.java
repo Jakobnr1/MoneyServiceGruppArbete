84e09af8c5bf0a788325976c029f0169c19e2112
LindbergsKarl <karl.lindberg1@yaelev.se>
2021-04-12 10:20:16 +0200

    ClientApp.class skapad

---
+++ src/MoneyService/ClientApp.java
@@ -0,0 +1,30 @@
+package MoneyService;
+
+public class ClientApp {
+
+	private final long id;
+	PayService info;
+	
+	public ClientApp(long id, PayService info) {
+		super();
+		this.id = id;
+		this.info = info;
+	}
+
+	public static void chooseSite() {
+		
+	}
+	
+	public static void showExchangeRates() {
+		
+	}
+	
+	public static Order createOrder() {
+		
+		
+		return null;
+	}
+	
+	
+	
+}
