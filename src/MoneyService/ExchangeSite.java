abab9f894b8127c7ba252bb7d46377a09f509235
LindbergsKarl <karl.lindberg1@yaelev.se>
2021-04-12 10:05:00 +0200

    ExchangeSite.class skapad
    Har lagt in lite testmetoder för att sätta kurs, köp och sälj

---
+++ src/MoneyService/ExchangeSite.java
@@ -0,0 +1,120 @@
+package MoneyService;
+
+import java.util.EnumMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import MoneyService.Currency.CurrencyName;
+
+public class ExchangeSite {
+
+	private final long id;
+	Map<String,Float> exchangeRates;
+	
+	public ExchangeSite(long id, Map<String, Float> exchangeRates) {
+		super();
+		this.id = id;
+		this.exchangeRates = exchangeRates;
+	}
+
+	public static void setRates(String currencieToChange, double byRate, double sellRate, EnumMap<CurrencyName, List<Banknote>> currenieList) {
+
+		Set<CurrencyName> currencysKeys = currenieList.keySet();
+
+		Iterator<CurrencyName> keyIter = currencysKeys.iterator();
+
+		while(keyIter.hasNext()) {
+			CurrencyName key= keyIter.next();
+			String temp=key.toString();
+
+			if(temp.equalsIgnoreCase(currencieToChange)) {
+				System.out.println("test");
+				key.setByRate(byRate);
+				key.setSellRate(sellRate);
+			}
+		}		
+	}
+	
+	public static boolean parseOrder() {
+		
+		return false;
+		
+		
+	}
+	
+	public static boolean sellCurrency(EnumMap<CurrencyName, List<Banknote>> currencyList, String currencyToChange, int denomination, int newNumberOfNotes) {
+
+		boolean okChange = false;
+		Set<CurrencyName> currencysKeys = currencyList.keySet();
+
+		Iterator<CurrencyName> keyIter = currencysKeys.iterator();
+
+		while(keyIter.hasNext()) {
+			CurrencyName key= keyIter.next();
+			String temp=key.toString();
+
+			if(temp.equalsIgnoreCase(currencyToChange)) {
+				
+				for(int i=0; i<currencyList.get(key).size(); i++) {
+					if(currencyList.get(key).get(i).getDenomination() == denomination) { //Searching for correct denomination to change.
+						if(currencyList.get(key).get(i).setNumberOfNotes(newNumberOfNotes)) {//Updates the numberOfNotes 
+							return okChange = true;
+							
+						}
+						
+					}	
+				}
+			}
+		}
+		return okChange;
+	}
+	
+	public static boolean byCurrency(EnumMap<CurrencyName, List<Banknote>> currencyList, String currencyToChange, int denomination, int newNumberOfNotes) {
+
+		boolean okChange = false;
+		Set<CurrencyName> currencysKeys = currencyList.keySet();
+
+		Iterator<CurrencyName> keyIter = currencysKeys.iterator();
+
+		while(keyIter.hasNext()) {
+			CurrencyName key= keyIter.next();
+			String temp=key.toString();
+
+			if(temp.equalsIgnoreCase(currencyToChange)) {
+				
+				for(int i=0; i<currencyList.get(key).size(); i++) {
+					if(currencyList.get(key).get(i).getDenomination() == denomination) { //Searching for correct denomination to change.
+						if(currencyList.get(key).get(i).setNumberOfNotes(newNumberOfNotes)) {//Updates the numberOfNotes 
+							return okChange = true;
+							
+						}
+						
+					}	
+				}
+			}
+		}
+		return okChange;
+	}
+	
+	public Map<String, Float> getExchangeRates() {
+		return exchangeRates;
+	}
+
+	public void setExchangeRates(Map<String, Float> exchangeRates) {
+		this.exchangeRates = exchangeRates;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("ExchangeSite [id=%s, exchangeRates=%s]", id, exchangeRates);
+	}
+	
+	
+	
+}
