81dc207487fad21d7383201e99c17ab192a118b3
LindbergsKarl <karl.lindberg1@yaelev.se>
2021-04-14 17:29:51 +0200

    Changed Currency and MoneyBox

--- src/MoneyService/MoneyBox.java
+++ src/MoneyService/MoneyBox.java
@@ -1,6 +1,5 @@
 package MoneyService;
 
-import java.util.List;
 import java.util.Map;
 
 public class MoneyBox implements java.io.Serializable {
@@ -8,28 +7,40 @@ public class MoneyBox implements java.io.Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private final long id;
-	Map<String, List<Currency>> currencyMap;
+	Map<String, Currency> currencyMap;
 
 
-	public MoneyBox(long id, Map<String, List<Currency>> currencyMap) {
+	public MoneyBox(long id, Map<String, Currency> currencyMap) {
 		super();
 		this.id = id;
 		this.currencyMap = currencyMap;
 	}
 
-	void orderPickup(){
-		
-		
-	}
-	
-	public Map<String, List<Currency>> getCurrencyList() {
+
+
+	public Map<String, Currency> getCurrencyMap() {
 		return currencyMap;
 	}
 
-	
+
+
+	public void setCurrencyMap(Map<String, Currency> currencyMap) {
+		this.currencyMap = currencyMap;
+	}
+
+
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+
+
 	public long getId() {
 		return id;
 	}
+
+
 
 	@Override
 	public String toString() {
