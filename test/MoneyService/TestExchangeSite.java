bc434cd8f6019704c4fe09bce4b30126ca76ae26
Jakob Solberg <jakob-nr1@hotmail.com>
2021-04-15 17:25:18 +0200

    Started a bit more on Exchangesite and testExchangesite

---
+++ test/MoneyService/TestExchangeSite.java
@@ -0,0 +1,20 @@
+package MoneyService;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestExchangeSite {
+
+	
+	
+	@Test
+	public void testBuyMoney() {
+		Order orderData = new Order(500,"EUR","BUY");
+		
+		
+		
+		fail("Not yet implemented");
+	}
+
+}
